9c3fde6535159de3db5cbf2274ff50fb7774c798
reiko_iguchi <reiko6122@gmail.com>
2022-10-07 08:58:26 +0900

    2022/10/07

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(CareLevelsTableSeeder::class);
-        $this->call(DayofweeksTableSeeder::class);
         $this->call(ServiceTypesTableSeeder::class);
     }
 }
--- database/seeders/ServiceTypesTableSeeder.php
+++ database/seeders/ServiceTypesTableSeeder.php
@@ -17,12 +17,7 @@ class ServiceTypesTableSeeder extends Seeder
         $service_types = [
             ['name' => '通所介護（デイサービス）'],
             ['name' => '通所リハビリテーション（デイケア）'],
-            ['name' => '訪問介護'],
             ['name' => '訪問看護'],
-            ['name' => '訪問リハビリテーション'],
-            ['name' => '短期入所生活介護（ショートステイ）'],
-            ['name' => '短期入所療養介護（ショートステイ）'],
-            ['name' => '福祉用具貸与'],
             ['name' => '居宅療養管理指導']
         ];
         foreach ($service_types as $service_type) {
