23a5c9628aafa34f0ede59a1169d858c98e7e198
reiko_iguchi <reiko6122@gmail.com>
2022-10-08 22:23:34 +0900

    2022/10/08

---
+++ app/Http/Requests/NursingCareOffice/UpdateRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests\NursingCareOffice;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'office_name' => 'required|string|max:255',
+            'corporate_name' => 'required|string|max:255',
+            'service_type_id' => 'required|numeric',
+            'office_number' => 'required|size:10',
+            'post_code' => 'required|size:7',
+            'address' => 'required|string|max:255',
+            'name' => 'required|string|max:255',
+            'name_furigana' => 'required|string|max:255',
+            'email' => 'required|email|unique:nursing_care_offices',
+            'tel' => 'required|between:10,11|regex:/^0[0-9]{10,11}$/',
+            'password' => 'nullable|between:8,64|confirmed|regex:/^[a-zA-Z0-9]+$/'
+        ];
+    }
+}
